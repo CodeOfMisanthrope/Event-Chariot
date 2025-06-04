6c6483642ae2da29d7badd1df79de73edded8828
Ilya <iosdsbb@gmail.com>
2025-06-04 23:35:15 +0300

    fix: StoreHandler set

--- src/store/interfaces.ts
+++ src/store/interfaces.ts
@@ -3,7 +3,7 @@ import {Handler} from "~handler";
 export interface StoreHandler<T> {
   get(key: string): Nullable<Iterable<Handler<T>>>;
 
-  set(key: string, val: Handler<T>): void;
+  set(key: string, handler: Handler<T>): void;
 
   clear(): void;
 
