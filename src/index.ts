e6a338eb80b7b227326e518e17a5d7ab4b572fb9
Ilya <iosdsbb@gmail.com>
2025-06-04 23:45:52 +0300

    feat: added on, _on to EventEmitter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {StoreHandler} from "~store/interfaces";
 import {HandeStoreDefault} from "~store";
+import {Handler} from "~handler";
 
 console.log("Start...");
 
@@ -12,5 +13,13 @@ class EventEmitter<T> {
   ) {
     this._captures = new HandeStoreDefault();
     this._bubbles = new HandeStoreDefault();
+  }
+
+  protected _on(event:string, handler:Handler<T>, store:StoreHandler<T> = this._bubbles) {
+    store.set(event, handler);
+  }
+
+  public on(event: string, handler: Handler<T>) {
+    this._on(event, handler);
   }
 }
